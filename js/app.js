140328104d5f322d4d9dc0d69c947c9646adc8a8
SudhinRaj <58643406+rjsudhin@users.noreply.github.com>
2023-11-05 11:18:02 +0530

    logos are added button and heading

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const addItemButton = document.querySelector('#add-me')
 const footer = document.querySelector('footer')
 const resetButton = document.createElement('button')
 const listRecord = []
+const colors = ['orange', 'skyblue', 'pink', '#fff']
 
 addItemButton.addEventListener('click', () => {
     const cartItems = userInput.value
@@ -13,14 +14,27 @@ addItemButton.addEventListener('click', () => {
         alert("please enter something on the input field")
 
     } else if (userInput.value != '') {
+        let color = Math.floor(Math.random() * colors.length)
+        console.log(color)
         const li = document.createElement('li')
         const span = document.createElement('span')
-        const btn = document.createElement('button')
+        const btn = document.createElement('span') 
+    
+        btn.classList.add("material-symbols-outlined")
+        btn.style.color = 'rgb(143, 126, 172)'
+        btn.style.cursor = 'pointer'
+        // btn.style.border = '1px solid rgb(143, 126, 172)'
+        btn.style.borderRadius = '50%'
+        btn.style.padding = '2px'
+        btn.style.margin = '3px'
+
 
         li.appendChild(span)
         span.textContent = cartItems
+        span.style.color = colors[color]
+        console.log(span)
         li.appendChild(btn)
-        btn.textContent = 'delete'
+        btn.textContent = 'backspace'
         userList.appendChild(li)
         listRecord.push(cartItems)
         outputSection.appendChild(userList)
